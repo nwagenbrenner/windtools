30bed393a27b4686c345d02ad80ebb07098958a5
nwagenbrenner <nwagenbrenner@gmail.com>
2013-07-24 15:19:58 -0600

    change speed subsetting

--- R/plotVectors.R
+++ R/plotVectors.R
@@ -22,12 +22,12 @@ readData <- function(fileName){
 #=======================================================
 #     plot speed time series for a single sensor
 #=======================================================
-plotSensorSpeed <- function(sensor){
-    s<-subset(speed, subset=(plot == sensor))
-    s[,"datetime"] <- as.character(s[,"datetime"])
-    s[,"datetime"] <- as.POSIXct(strptime(s[,"datetime"], '%Y-%m-%d %H:%M:%S'))
+plotSensorSpeed <- function(df, sensor){
+    df<-subset(df, subset=(plot == sensor))
+    df[,"datetime"] <- as.character(df[,"datetime"])
+    df[,"datetime"] <- as.POSIXct(strptime(df[,"datetime"], '%Y-%m-%d %H:%M:%S'))
 
-    p<-ggplot(s, aes(x=datetime, y=obs_speed)) +
+    p<-ggplot(df, aes(x=datetime, y=obs_speed)) +
         geom_point(shape=19, size=1.5, alpha = 1) +
         geom_line() +
         #geom_smooth(method=loess) +
@@ -44,23 +44,25 @@ plotSensorSpeed <- function(sensor){
 #=======================================================
 #     subset on speed criteria
 #=======================================================
-#make df with just NM1 data for subsetting the larger df
-subsetOnSpeed <- function(df, sensor, threshold){
-    nm1<-subset(df, subset=(plot == sensor & obs_speed > threshold))
+subsetOnSpeed <- function(df, sensor, condition, threshold){
+    if(condition == '>'){
+        s<-subset(df, subset=(plot == sensor & obs_speed > threshold))
+    }
+    else if(condition == '<'){
+        s<-subset(df, subset=(plot == sensor & obs_speed < threshold))
+    }
     speedTest <- rep(NA, length(df$datetime)) #vector of T/F
-
-    for (i in 1:length(df$datetime)){ 
-        speedTest[i] <- any(nm1$datetime == speed$datetime[i]) #True if R2 obs_speed < threshold
+    
+    for(i in 1:length(df$datetime)){ 
+        speedTest[i] <- any(s$datetime == df$datetime[i]) #True if sensor obs_speed meets threshold condition
     }
 
     df<-cbind(df,speedTest)
     df<-subset(df, subset=(df$speedTest == TRUE))
-    df[,"datetime"] <- as.factor(df[,"datetime"])
-    #df<-subset(df, subset=(obs_speed >= 0.0)) #get rid of -888.0
-    df[,"plot"] <- as.factor(df[,"plot"])
+    df <- subset(df, select = -speedTest) #drop speedTest from df
 
-    obs_dir_radians <- df$obs_dir * pi/180 #convert to radians
-    df <- cbind(df, obs_dir_radians)
+    df[,"datetime"] <- as.factor(df[,"datetime"])
+    df[,"plot"] <- as.factor(df[,"plot"])
     
     return(df)
 }
@@ -69,8 +71,12 @@ subsetOnSpeed <- function(df, sensor, threshold){
 #  Build df with hourly avg speeds
 #============================================================
 buildHourlyAverages <- function(df){
+    obs_dir_radians <- df$obs_dir * pi/180 #convert to radians
+    df <- cbind(df, obs_dir_radians)
+
     hrSpeed<-data.frame(rbind(rep(NA,8)))
     names(hrSpeed)<-c("obs_speed", "obs_dir", "lat", "lon", "plot", "u", "v", "hour")
+    
     for (i in 0:23){
         hour<-subset(df, subset=(as.POSIXlt(datetime)$hour == i))
     
@@ -80,21 +86,23 @@ buildHourlyAverages <- function(df){
         latAvg<-tapply(hour$lat, hour$plot, mean)
         lonAvg<-tapply(hour$lon, hour$plot, mean)
         dirAvg<-dirAvgRad * 180/pi
+        
         for (m in 1:length(dirAvg)){
             if(!is.na(dirAvg[m]) && dirAvg[m] < 0.0){
                 dirAvg[m]<-dirAvg[m] + 360.0
             }
         }
+        
         hourlyAvg<-as.data.frame(cbind(spdAvg, dirAvg, latAvg, lonAvg))
         plot<-rownames(hourlyAvg)
         hourlyAvg<-as.data.frame(cbind(hourlyAvg, plot))
         row.names(hourlyAvg) <- NULL
+        
         # calc u, v for avg speeds and add to speed df
         u<-mapply(speed2u, hourlyAvg$spdAvg, hourlyAvg$dirAvg)
         v<-mapply(speed2v, hourlyAvg$spdAvg, hourlyAvg$dirAvg)
         hourlyAvg<-as.data.frame(cbind(hourlyAvg,u,v,i))
         colnames(hourlyAvg)<-c('obs_speed', 'obs_dir', 'lat', 'lon', 'plot', 'u', 'v', 'hour')
-        #rbind this hour's avg data to the full ds
         hrSpeed<-rbind(hrSpeed, hourlyAvg)
     }
 
@@ -118,14 +126,8 @@ subsetOnHour <- function(df, h1, h2, h3, h4){
 #=======================================================
 #    vector field
 #=======================================================
-makeVectorMap <- function(df, site){
-    if(site == 'bsb'){
-        myMap<-get_map(location = c(lon = -113.027724, lat = 43.402726), zoom = 12, maptype = 'terrain')
-    }
-    else if(site == 'src'){
-        myMap<-get_map(location = c(lon = -116.228889, lat = 45.401667), zoom = 14, maptype = 'terrain')
-    }
-
+makeVectorMap <- function(df, lat, lon, zoom, maptype){
+    myMap<-get_map(location = c(lon=lon, lat=lat), zoom=zoom, maptype=maptype)
     #note that xend,yend directions are reversed bc of weird issue with arrow (only plots correctly with ends=first)
     #line segements centered on sensor location
     p <- ggmap(myMap)
