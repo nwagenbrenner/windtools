48700444c1b48dc5af535b86fa2bee3c7f9068ac
nwagenbrenner <nwagenbrenner@gmail.com>
2013-08-09 14:55:12 -0600

    add kml.R

--- R/db.R
+++ R/db.R
@@ -1,9 +1,9 @@
 #=======================================================
 #    Fetch all data from a wind database
 #=======================================================
-#' @title Fetch all data from a database
+#' @title Fetch all data from a database for specified time period
 #' @description
-#' \code{dbFetchAll} returns a dataframe of wind data 
+#' \code{dbFetchAll} returns a dataframe of wind data for specified time period
 #' @param dbDrv database driver ("SQLite" is only option right now)
 #' @param db database to query
 #' @param start_time format is '2011-08-15T06:00:00'
@@ -11,8 +11,8 @@
 #' @return dataframe with id, date/time, speed, gust, direction, and quality
 #' @export
 #' @details
-#' This fucntion returns a dataframe of raw, 30-s wind data. Currently only 
-#' connects to SQLite db. 
+#' This fucntion returns a dataframe of raw, 30-s wind data for a specified
+#' time period. Currently only connects to SQLite db. 
 
 dbFetchAll <- function(dbDrv, db, start_time, end_time){
     stopifnot(require("RSQLite"))
---
+++ R/kml.R
@@ -0,0 +1,23 @@
+#=======================================================
+#    Write a kml from raster
+#=======================================================
+#' @title Write a kml from a raster object
+#' @description
+#' \code{raster2kml} writes a kml from a raster object
+#' @param r raster object 
+#' @param outfile output file name
+#' @export
+#' @details
+#' This fucntion writes a kml representation of a raster object.
+#' @examples
+#' r <- raster(ncol=10, nrow=10)
+#' values(r) <- 1:ncell(r)
+#' extent(r)<-c(-113.5732, -112.4852, 43.04848, 43.81728)
+#' raster2kml(r, 'test_kml')
+
+raster2kml <- function(r, outfile){
+    stopifnot(require(plotKML))
+    newproj <- "+proj=longlat +datum=WGS84"
+    r2<-projectRaster(r, crs=newproj, method="bilinear") #convert to lat/lon CRS
+    KML(r2, outfile, col=rev(terrain.colors(255)),colNA=NA, maxpixels=100000, blur=1, zip='', overwrite=TRUE)
+}
