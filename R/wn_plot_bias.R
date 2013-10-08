da68191745127ef5b8f60b54d8eb361a79e36576
natalie <nwagenbrenner@gmail.com>
2013-10-07 22:14:51 -0600

    fix faceted time series plots

--- R/wn_plot_bias.R
+++ R/wn_plot_bias.R
@@ -5,81 +5,97 @@
 #' @description
 #' \code{wnPlotSpeedTs} plots wind speed time series faceted on sensor and weather model type
 #' @param df dataframe returned from wnBuildTimeSeriesDf()
+#' @param color_list optional list of colors to use (e.g., colors <- c("WindNinja" = "red", "NWP" = "darkblue"))
+#' @param xscale optional scale_x_datetime() argument
 #' @return ggplot2 object
 #' @export
 #' @details
-#' Plots the wind speed time series faceted on weather model type if the supplied dataframe
-#' contains a single sensor. If the dataframe contains multiple sensors, the time series plot
-#' is facect on sensor and weather model type.
-wnPlotSpeedTs <- function(df){
+#' Plots the wind speed time series faceted on NWP model and model type 
+wnPlotSpeedTs <- function(df, color_list, xscale=NULL){
     stopifnot(require("ggplot2"))
-    p<-ggplot(df, aes(x=datetime, y=pred_speed, colour=fcastType)) +
-        geom_line(linetype="dashed") + 
-        geom_point(shape=19, size=1.5, alpha = 1.0) +
-        xlab("Time") + ylab("Speed (m/s)") +
-        scale_colour_brewer(name='Model', palette='Set1')
+    p <- ggplot(df, aes(x=datetime, y=obs_speed)) +
+        geom_point(shape=19, size=1.5, alpha = 1.0, colour='black', position="jitter") +
+        xlab("Time") + ylab("Speed (m/s)")
 
-    p<-p + theme(axis.text.x = element_text(angle = 45))
-    p<-p + theme(axis.text.x = element_text(vjust = 0.5))
-    #p<-p + theme(axis.text.x = element_text(hjust = 0.2))
-    p<-p + geom_line(data=df, aes(x=datetime, y=obs_speed), colour='black')
-    p<-p + geom_point(data=df, aes(x=datetime, y=obs_speed),shape = 19, size=1.5, colour='black', alpha = 1.0)
-
-    p<-p + scale_x_datetime(breaks=c(min(df$datetime)+12*60*60,
-                       min(df$datetime)+12*3*60*60,
-                       min(df$datetime)+12*5*60*60,
-                       min(df$datetime)+12*7*60*60,
-                       min(df$datetime)+12*9*60*60))
-    if(length(unique(df$plot)) == 1){
-        p<-p + ggtitle(df$plot[1])
-        p<-p + facet_wrap( ~ wxType, ncol = 1)
+    p <- p + theme(axis.text.x = element_text(angle = 45))
+    p <- p + theme(axis.text.x = element_text(vjust = 0.5))
+    
+    if(is.null(xscale)){
+        #try to set reasonable breaks for x-axis
+        p<-p + scale_x_datetime(breaks=c(min(df$datetime)-1*60*60,
+                       min(df$datetime)+5*60*60,
+                       min(df$datetime)+11*60*60,
+                       min(df$datetime)+17*60*60))
+        #p<-p + scale_x_datetime(breaks=c(min(df$datetime)+12*60*60,
+      #                 min(df$datetime)+12*3*60*60,
+      #                 min(df$datetime)+12*5*60*60,
+      #                 min(df$datetime)+12*7*60*60,
+      #                 min(df$datetime)+12*9*60*60))
     }
     else{
-        p<-p + facet_grid(wxType ~ plot)
+        p <- p + xscale
     }
+
+    p<-p + geom_point(data=df, aes(x=datetime, y=pred_speed, colour=fcastType),shape = 19, size=1.5, alpha = 0.5, position="jitter")
+    #p<-p + geom_smooth(method="loess", aes(x=datetime, y=pred_speed, colour=fcastType))
+    p<-p + scale_colour_manual(values=color_list, name="Model Type")
+    p<-p + facet_grid(fcastType ~ wxType)
+
+    if(length(unique(df$plot)) == 1){
+        p<-p + ggtitle(df$plot[1])
+    }
+
     return(p)
 }
 
 
 #=======================================================
-#  wind direction time series plots faceted on wx type
+#  faceted wind direction time series plots
 #=======================================================
 #' @title Make a faceted wind direction time series plot
 #' @description
 #' \code{wnPlotDirTs} plots wind direction time series faceted on sensor and weather model type
 #' @param df dataframe returned from wnBuildTsDf()
+#' @param color_list optional list of colors to use (e.g., colors <- c("WindNinja" = "red", "NWP" = "darkblue"))
+#' @param xscale optional scale_x_datetime() argument
 #' @return ggplot2 object
 #' @export
 #' @details
-#' Plots the wind direction time series faceted on weather model type if the supplied dataframe
-#' contains a single sensor. If the dataframe contains multiple sensors, the time series plot
-#' is facect on sensor and weather model type.
-wnPlotDirTs <- function(df){
+#' Plots the wind direction time series faceted on NWP type and model type
+wnPlotDirTs <- function(df, color_list, xscale=NULL){
     stopifnot(require("ggplot2"))
-    p<-ggplot(df, aes(x=datetime, y=pred_dir, colour=fcastType)) +
-        geom_line(linetype="dashed") + 
-        geom_point(shape=19, size=1.5, alpha = 1.0) +
-        xlab("Time") + ylab("Direction") +
-        scale_colour_brewer(name='Model', palette='Set1')
+    p <- ggplot(df, aes(x=datetime, y=obs_dir)) +
+        geom_point(shape=19, size=1.5, alpha = 1.0, colour='black', position="jitter") +
+        xlab("Time") + ylab("Direction")
 
-    p<-p + theme(axis.text.x = element_text(angle = 45))
-    p<-p + theme(axis.text.x = element_text(vjust = 0.5))
-    #p<-p + theme(axis.text.x = element_text(hjust = 0.2))
-    p<-p + geom_line(data=df, aes(x=datetime, y=obs_dir), colour='black')
-    p<-p + geom_point(data=df, aes(x=datetime, y=obs_dir),shape = 19, size=1.5, colour='black', alpha = 1.0)
-
-    p<-p + scale_x_datetime(breaks=c(min(df$datetime)+12*60*60,
-                       min(df$datetime)+12*3*60*60,
-                       min(df$datetime)+12*5*60*60,
-                       min(df$datetime)+12*7*60*60,
-                       min(df$datetime)+12*9*60*60))
-    if(length(unique(df$plot)) == 1){
-        p<-p + ggtitle(df$plot[1])
-        p<-p + facet_wrap( ~ wxType, ncol = 1)
+    p <- p + theme(axis.text.x = element_text(angle = 45))
+    p <- p + theme(axis.text.x = element_text(vjust = 0.5))
+    
+    if(is.null(xscale)){
+        #try to set reasonable breaks for x-axis
+        p<-p + scale_x_datetime(breaks=c(min(df$datetime)-1*60*60,
+                       min(df$datetime)+5*60*60,
+                       min(df$datetime)+11*60*60,
+                       min(df$datetime)+17*60*60))
+        #p<-p + scale_x_datetime(breaks=c(min(df$datetime)+12*60*60,
+      #                 min(df$datetime)+12*3*60*60,
+      #                 min(df$datetime)+12*5*60*60,
+      #                 min(df$datetime)+12*7*60*60,
+      #                 min(df$datetime)+12*9*60*60))
     }
     else{
-        p<-p + facet_grid(wxType ~ plot)
+        p <- p + xscale
     }
+
+    p<-p + geom_point(data=df, aes(x=datetime, y=pred_dir, colour=fcastType),shape = 19, size=1.5, alpha = 0.5, position="jitter")
+    #p<-p + geom_smooth(method="loess", aes(x=datetime, y=pred_speed, colour=fcastType))
+    p<-p + scale_colour_manual(values=color_list, name="Model Type")
+    p<-p + facet_grid(fcastType ~ wxType)
+
+    if(length(unique(df$plot)) == 1){
+        p<-p + ggtitle(df$plot[1])
+    }
+
     return(p)
 }
 
@@ -167,28 +183,33 @@ wnPlotBiasVsObs <- function(df, var){
 #' \code{wnPlotObsVsPred} plots observed vs. predicted data
 #' @param df dataframe returned from wnBuildBiasDf()
 #' @param var variable to plot ('speed' or 'direction')
+#' @param color_list optional list of colors to use for legend
 #' @return ggplot2 object
 #' @export
 #' @details
 #' Returns a scatter plot of observed vs predicted speed or direction data.
-wnPlotObsVsPred <- function(df, var){
+wnPlotObsVsPred <- function(df, var, color_list=NULL){
     stopifnot(require("ggplot2"))
     if(var == 'speed'){
-        p<-ggplot(data, aes(x=obs_speed, y=pred_speed, colour=fcastName)) +
+        p<-ggplot(data, aes(x=obs_speed, y=pred_speed, linetype = fcastTypeOrdered, colour=fcastNameOrdered)) +
             xlab("Observed Speed (m/s)") + ylab("Predicted Speed (m/s)")
     }
     else if(var == 'direction'){
-        p<-ggplot(data, aes(x=obs_dir, y=pred_dir, colour=fcastName)) +
+        p<-ggplot(data, aes(x=obs_dir, y=pred_dir, linetype = fcastTypeOrdered, colour=fcastNameOrdered)) +
             xlab("Observed Direction (m/s)") + ylab("Predicted Direction (m/s)")
     }
     else{
         stop('Incorrect variable specified for var. Options are \'speed\' or \'direction\'.')
     }
     p <- p + geom_point(shape=19, size=1.5, alpha = 0.5) +
-            geom_smooth(method=loess) +
-            scale_colour_brewer(palette='Set1') +
+            geom_smooth(method=loess, size=0.75) +
+            scale_colour_brewer(palette='Set1', name="Model") +
             theme_bw() +
-            geom_abline(intercept=0, slope=1, linetype='dashed')
+            geom_abline(intercept=0, slope=1, linetype='dashed') +
+            scale_y_continuous(limits = c(-0.05, 14))
+            if(!is.null(color_list)){
+             p <- p + scale_colour_manual(values = color_list, name="Model")
+            }
             
     return(p)
 }
