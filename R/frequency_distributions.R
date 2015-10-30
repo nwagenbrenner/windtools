5769916c4d3d1a20eb44da90f736d84f19b5389a
nwagenbrenner <nwagenbrenner@gmail.com>
2015-10-30 12:34:30 -0600

    update freq dist stuff and docs

--- R/frequency_distributions.R
+++ R/frequency_distributions.R
@@ -11,35 +11,34 @@
 #' @export
 #' @details
 #' This function crates a lookup table of wind directions
-#' for use in generating wind direction distributions/
+#' for use in generating wind direction distributions.
 #'
-#' @examples
 
 createLookUpTable <- function(df, pathToRasters, rasterNames){
-
+    stopifnot(require("raster"))
+    stopifnot(require("sp"))
     #spatial points to extract data from raster
     r<-raster(paste0(pathToRasters, rasterNames[1]))
     points<-(cbind(df$lon,df$lat))
     sp <- SpatialPoints(points, proj4string=CRS("+proj=longlat +datum=WGS84"))
-    sp <- spTransform(sp, CRS(proj4string(r)))
+    sp <- spTransform(sp, CRS(proj4string(r)))  
 
-    directions<-seq(0, 359, by=22.5)
     #loop over the raster files
-    for(i in directions){
+    for(i in 1:length(rasterNames)){
         print (i)
-        i<-round(i+0.1) #0.5 can go either way, 0 or 1
-        path<-paste0(pathToRasters, rasterNames[i])
-        print (path)
+        path<-paste0(pathToRasters, rasterNames)[i]
+        print(path)
         r<-raster(path)
         #extract the data from the raster object
         assign(paste0("e_", i), extract(r, sp, method='simple')) #nearest-neighbor
         #add new data to dataframe
-        d<-cbind(d, get(paste0("e_",i)))
+        df<-cbind(df, get(paste0("e_",i)))
     }
 
-    colnames(d)<-c("plot","lat","lon","0","23","45","68","90","113","135","158",
+    colnames(df)<-c("plot","lat","lon","0","23","45","68","90","113","135","158",
                "180","203","225","248","270","293","315","338")
-    return(d)
+
+    return(df)
 }
 
 #=======================================================
@@ -48,31 +47,34 @@ createLookUpTable <- function(df, pathToRasters, rasterNames){
 #' @title Generate wind direction distribution 
 #' @description
 #' \code{distributionFromTable} returns a wind direction distribution
-#' @param v vector of observed wind directions
+#' @param v vector of binned observed wind directions (number of bins has to equal the number of bins in table) 
 #' @param table the lookup table to use
 #' @return dataframe of wind direction distributions
 #' @export
 #' @details
-#'
-#' @examples
+#' This function generates a distribution of wind directions
+#' based on a lookup table and vector of observed directions.
+
 
 distributionFromTable <- function(v, table){
-    
-    bin<-round(v + 0.1) #since 0.5 could go to 0 or 1
 
-    dist<-data.frame(rbind(rep(NA,2)))
-
-    #create the direction distributions (f1_dir, f2_dir, f3_dir, etc.)
-    for(i in 1:length(table$plot)){
-        assign(paste0(table$plot,'_dir'),(mapply(lookUpDir, bin, i)))
-        dist<-rbind(dist, paste0(table$plot,'_dir'))
-    }
+    dist<-data.frame(rbind(rep(NA,1)))
 
     #look up direction from table
     lookUpDir<-function(bin, rowindex){  
         dir<-table[,as.character(eval(bin))][rowindex]
         return(dir)
     }
+
+    #create the direction distributions (f1_dir, f2_dir, f3_dir, etc.)
+    for(i in 1:length(table$plot)){
+        assign(paste0(table$plot[i],'_dir'),(mapply(lookUpDir, v, i)))
+        dist<-cbind(dist, get(paste0(table$plot[i],'_dir')))
+    }
+    
+    dist[1]<-NULL #drop the column of NAs
+    colnames<-paste0(table$plot,'_dir')
+    colnames(dist)<-colnames
 
     return(dist)
 }
